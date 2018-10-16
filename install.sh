a670d86bddb63db165a1e872d8da16808a2756ac
Matthew Goldsmith <31556412+mgoldsmith1@users.noreply.github.com>
2018-10-16 14:10:00 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,8 +3,7 @@
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
 
     # Install some custom requirements on OS X
-    # e.g. 
-    brew install pyenv-virtualenv
+    # e.g. brew install pyenv-virtualenv
 
     case "${TOXENV}" in
         py36)
